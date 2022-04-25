051453c7fd185e05ff99557b1b3b041b0a470cc0
arkteck <arkteck@gmail.com>
2022-04-25 12:30:17 -0400

    Completed exercise: react components

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-// TODO
+var groceryList = ['mangos', 'watermelons', 'bananas', 'apples', 'honeydews'];
+
+var GroceryList = (props) => (
+  <ul>
+    {props.items.map(item => <GroceryListItem item={item} />)}
+  </ul>
+);
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bought: false,
+      hover: false
+    };
+  }
+  onListItemClick() {
+    this.setState({
+      bought: !this.state.bought
+    })
+  }
+
+  onListItemOver() {
+    this.setState({
+      hover: true
+    })
+  }
+  onListItemOut() {
+    this.setState({
+      hover: false
+    })
+  }
+  render() {
+    var style = {
+      textDecoration: this.state.bought ? 'line-through': 'none',
+      fontWeight: this.state.hover ? 'bold' : 'normal'
+    };
+    return <li style={style} onClick={this.onListItemClick.bind(this)} onMouseOver={this.onListItemOver.bind(this)} onMouseOut={this.onListItemOut.bind(this)}>{this.props.item}</li>;
+  }
+}
+
+ReactDOM.render(<GroceryList items={groceryList}/>, document.getElementById('app'));
